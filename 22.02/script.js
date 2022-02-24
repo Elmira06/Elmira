aea8990777e7b5841734266a633fdca4ce6e62e3
Elmira06 <91625269+Elmira06@users.noreply.github.com>
2022-02-24 17:45:04 +0200

    Add files via upload

---
+++ 22.02/script.js
@@ -0,0 +1,63 @@
+let inputWidth = document.querySelector(".input_width");
+let inputHeight = document.querySelector(".input_height");
+let inputColor = document.querySelector(".input_color");
+
+let btnCreate = document.querySelector(".btn_create");
+
+let btnUp = document.querySelector(".arrow_up");
+let btnDown = document.querySelector(".arrow_down");
+let btnLeft = document.querySelector(".arrow_left");
+let btnRight = document.querySelector(".arrow_right");
+
+let newDiv;
+let mTop = 0;
+let mLeft = 0;
+let iwValue;
+let ihValue;
+let isDiv = false;
+
+btnCreate.addEventListener('click', createDiv);
+
+
+function createDiv() {
+    if (!isDiv) {
+        iwValue = inputWidth.value;
+        ihValue = inputHeight.value;
+        icValue = inputColor.value;
+    
+        if (iwValue >= 50 && ihValue >= 50 && iwValue <= 500 && ihValue <= 500) {
+            
+            newDiv = document.createElement('div');
+            newDiv.style.width = iwValue + 'px';
+            newDiv.style.height = ihValue + 'px';
+            newDiv.style.backgroundColor = icValue; 
+        }
+
+        document.querySelector('.container').prepend(newDiv);
+        inputHeight.setAttribute("readonly", 1);
+        inputWidth.setAttribute("readonly", 1);
+
+        isDiv = true;
+    }
+}
+
+
+btnUp.addEventListener('click', blockUp);
+btnDown.addEventListener('click', blockDown);
+btnLeft.addEventListener('click', blockLeft);
+btnRight.addEventListener('click', blockRight);
+
+
+function blockUp() {
+    if (mTop-10 >= 0) {
+        mTop -= 10;
+        newDiv.style.marginTop = mTop + 'px';
+    }
+}
+
+function blockDown() {
+    if (500-(Number(mTop)+Number(ihValue)+10) >=0) {
+        mTop += 10;
+        newDiv.style.marginTop = mTop + 'px';
+    }
+}
